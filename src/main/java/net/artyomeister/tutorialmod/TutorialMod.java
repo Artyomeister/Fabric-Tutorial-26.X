1d51723b58b4edc566471eaa1aa3dfca78bdb63e
AAKrylov <ArtAKrylov@mai.education>
2026-08-26 23:45:10 +0300

    Initial commit

---
+++ src/main/java/net/artyomeister/tutorialmod/TutorialMod.java
@@ -0,0 +1,22 @@
+package net.artyomeister.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.fromNamespaceAndPath(MOD_ID, path);
+	}
+}
